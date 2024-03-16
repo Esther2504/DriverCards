3b1a24a2d490731eea84ad2e19f008eaa1eabb5e
Esther2504 <esther252000@gmail.com>
2024-03-16 16:42:07 +0100

    mapping drivers on the page

--- script.tsx
+++ script.tsx
@@ -1,11 +1,36 @@
+let jsonContent;
+
+getDrivers();
+
+let container = document.getElementsByClassName('card-container')
+
+console.log(container)
+
 async function getDrivers() {
     try {
-        const response = await fetch('https://f1-api.vercel.app/api/drivers'); 
-        const jsonContent = await response.json();
+        const response = await fetch('https://f1-api.vercel.app/api/drivers');
+        jsonContent = await response.json();
         console.log(jsonContent);
-    
-        return jsonContent; 
-      } catch (error) {
+
+        
+        const drivers = jsonContent.map((driver, i) => {
+            return `<div class="driver-card" style="background:${driver['team-color']}" key=${i}>
+            <img src=${driver.image} alt="user profile image" />
+            <div class="details">
+            <img src=${driver['country-flag']} />
+            <img src=${driver['number-logo']} />
+             <p>${driver.name}</p>
+             <p>${driver.team}</p>
+             <p>Current rank: ${driver.rank}</p>
+             <p>Points: ${driver.points}</p>
+           </div>
+      </div>`;
+        })
+container[0].innerHTML = drivers
+
+        return jsonContent;
+    } catch (error) {
         console.error('Error fetching data:', error);
-      }
+    }
 }
+
