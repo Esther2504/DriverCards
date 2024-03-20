ff78e7239cf19613ee0fb7643d3156ae9b1b7898
Esther2504 <esther252000@gmail.com>
2024-03-20 12:09:53 +0100

    font, styling

--- script.tsx
+++ script.tsx
@@ -13,26 +13,26 @@ async function getDrivers() {
         console.log(jsonContent);
 
         
-        const drivers = jsonContent.map((driver, i) => {
-            return `<div class="driver-card" style="background:${driver['team-color']}" key=${i}>
-            <div>
+        const drivers = jsonContent.map((driver) => {
+            return `<div class="driver-card" style="background:${driver['team-color']}">
+            <div class="img-container">
             <img src=${driver.image} alt="${driver.name}" class="driver-img" />
             <img src=${driver['country-flag']} alt="flag" class="flag" />
             <img src=${driver['number-logo']} alt="driver number" class="number" />
             </div>
             <div class="details">
              <p>${driver.name}</p>
-             <p>${driver.team}</p>
-             <p>Current rank: ${driver.rank}</p>
-             <p>Points: ${driver.points}</p>
+             <p><span>Team</span> <span>${driver.team}</span></p>
+             <p><span>Current rank</span> <span>${driver.rank}</span></p>
+             <p><span>Points</span> <span>${driver.points}</span></p>
            </div>
       </div>`;
-        })
+        }).join('')
 container[0].innerHTML = drivers
 
         return jsonContent;
     } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching data:');
     }
 }
 
