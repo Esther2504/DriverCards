8f17d0768cc8120a959ed30f40e22716bfb4b781
Esther2504 <esther252000@gmail.com>
2024-03-21 20:44:15 +0100

    fetching more data onclick

--- script.tsx
+++ script.tsx
@@ -4,15 +4,13 @@ getDrivers();
 
 let container = document.getElementsByClassName('card-container')
 
-console.log(container)
-
 async function getDrivers() {
     try {
         const response = await fetch('https://f1-api.vercel.app/api/drivers');
         jsonContent = await response.json();
-
+       
         const drivers = jsonContent.sort().map((driver) => {
-            return `<div class="driver-card" style="background:${driver['team-color']}">
+            return `<div class="driver-card" style="background:${driver['team-color']}" onclick="moreInfo('${driver['last-name']}')">
             <div class="img-container">
             <img src=${driver.image} alt="${driver.name}" class="driver-img" />
             <img src=${driver['country-flag']} alt="flag" class="flag" />
@@ -29,9 +27,38 @@ async function getDrivers() {
 
         container[0].innerHTML = drivers
 
-        return jsonContent;
+
     } catch (error) {
         console.error('Error fetching data:');
     }
 }
 
+
+
+
+function moreInfo(driver) {
+    console.log(driver)
+    fetch('http://ergast.com/api/f1/2023/5/drivers')
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log(response.status);
+
+          return;
+        }
+        response.text().then(function(data) {
+            const xmlString = data
+            const parser = new DOMParser();
+const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+const familyNameElement = xmlDoc.getElementsByTagName('FamilyName')[0];
+
+const familyName = familyNameElement.textContent;
+          console.log(data);
+          console.log(familyName)
+        });
+      }
+    )
+    .catch(function(err) {
+      console.log('Fetch Error', err);
+    });
+}
