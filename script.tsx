8f455f3489d595543fc50d52eeadd246e08fcdc3
Esther2504 <esther252000@gmail.com>
2024-03-22 20:19:32 +0100

    fetch more driver info

--- script.tsx
+++ script.tsx
@@ -5,12 +5,12 @@ getDrivers();
 let container = document.getElementsByClassName('card-container')
 
 async function getDrivers() {
-    try {
-        const response = await fetch('https://f1-api.vercel.app/api/drivers');
-        jsonContent = await response.json();
-       
-        const drivers = jsonContent.sort().map((driver) => {
-            return `<div class="driver-card" style="background:${driver['team-color']}" onclick="moreInfo('${driver['last-name']}')">
+  try {
+    const response = await fetch('https://f1-api.vercel.app/api/drivers');
+    jsonContent = await response.json();
+
+    const drivers = jsonContent.sort().map((driver) => {
+      return `<div class="driver-card" style="background:${driver['team-color']}" onclick="moreInfo('${driver['last-name']}')">
             <div class="img-container">
             <img src=${driver.image} alt="${driver.name}" class="driver-img" />
             <img src=${driver['country-flag']} alt="flag" class="flag" />
@@ -23,42 +23,70 @@ async function getDrivers() {
              <p><span>Points</span> <span>${driver.points}</span></p>
            </div>
       </div>`;
-        }).join('')
+    }).join('')
 
-        container[0].innerHTML = drivers
+    container[0].innerHTML = drivers
 
 
-    } catch (error) {
-        console.error('Error fetching data:');
-    }
+  } catch (error) {
+    console.error('Error fetching data:');
+  }
 }
 
 
 
 
 function moreInfo(driver) {
-    console.log(driver)
-    fetch('http://ergast.com/api/f1/2023/5/drivers')
+  console.log(driver)
+  fetch(`http://ergast.com/api/f1/drivers/${driver}`)
     .then(
-      function(response) {
+      function (response) {
         if (response.status !== 200) {
           console.log(response.status);
 
           return;
         }
-        response.text().then(function(data) {
-            const xmlString = data
-            const parser = new DOMParser();
-const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
-const familyNameElement = xmlDoc.getElementsByTagName('FamilyName')[0];
+        response.text().then(function (data) {
+          const xmlString = data
+          const parser = new DOMParser();
+          const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+          const familyNameElement = xmlDoc.getElementsByTagName('FamilyName')[0];
+          getResults(driver);
 
-const familyName = familyNameElement.textContent;
+          const familyName = familyNameElement.textContent;
           console.log(data);
           console.log(familyName)
         });
       }
     )
-    .catch(function(err) {
+    .catch(function (err) {
+      console.log('Fetch Error', err);
+    });
+}
+
+function getResults(driver) {
+  console.log(driver)
+  fetch(`http://ergast.com/api/f1/2024/drivers/${driver}/results?limit=24`)
+    .then(
+      function (response) {
+        if (response.status !== 200) {
+          console.log(response.status);
+
+          return;
+        }
+        response.text().then(function (data) {
+          const xmlString = data
+          const parser = new DOMParser();
+          const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
+          const familyNameElement = xmlDoc.getElementsByTagName('FamilyName')[0];
+
+          const familyName = familyNameElement.textContent;
+          console.log(data);
+          console.log(familyName)
+        });
+      }
+    )
+    .catch(function (err) {
       console.log('Fetch Error', err);
     });
 }
